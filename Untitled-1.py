a4ec560f0190a441995d344c1a651e312d7b872e
Oleksandr Borysiuk <alexboris1004@gmail.com>
2026-03-14 19:25:57 +0100

    Lesson first

---
+++ Untitled-1.py
@@ -0,0 +1,30 @@
+# Lesson first
+print("Hello world!")
+print("I am Alex!")
+#name = input("Please enter your name: ")
+#print("Hi " + name)
+
+x = 3
+y = 4
+print(x, y)
+
+x,y = y,x
+print(x, y)
+print(type(x))
+print(x ** 3)
+print(x % 3)
+
+z = float(x)
+print(z)
+w = 1.3333
+print(int(w))
+print(round(w))
+
+flaut_num = 21
+
+entarance_num = (flaut_num - 1) // 20 + 1
+floor_num = (flaut_num - 1) % 20 //4 + 1
+print(entarance_num)
+print(floor_num)
+
+# Lesson second
