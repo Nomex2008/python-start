85c3daf46d0ee6daebdd9fa9f569781c9cf56e16
Oleksandr Borysiuk <alexboris1004@gmail.com>
2026-03-14 20:05:08 +0100

    Lesson second

--- Untitled-1.py
+++ Untitled-1.py
@@ -28,3 +28,49 @@ print(entarance_num)
 print(floor_num)
 
 # Lesson second
+
+t = True
+f = False
+
+print(t, f)
+print(t == f)
+print(t != f)
+
+x = 1
+print(x > -1 and x < 5)
+x = 6
+print(x > -1 and x < 5)
+
+print(not f)
+
+print('Bool')
+print(bool(0))
+print(bool(1))
+print(bool(-1))
+print(bool(10000))
+print(bool(''))
+print(bool('.'))
+
+print('If')
+if True:
+    print('Its true')
+
+x = 10
+if x > 0:
+    print('x more than 0')
+elif x < 0:
+    print('x less than 0')
+else:
+    print('x is 0')
+
+y = 4
+if y != 0 and y < 5:
+    print('y is smaller than 5 but not a zero')
+
+year = 2026
+if year % 4 == 0 and year % 100 != 0:
+    print('Year is leap')
+elif year % 400 == 0:
+    print('Year is leap')
+else:
+    print('Year is not leap')
